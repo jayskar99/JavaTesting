ec7f9a06dfbbe06f917aaa5cc8531a9cb9669e23
Jayden Cox <jayden_cox@tamu.edu>
2023-11-19 14:41:29 -0600

    all tests passing

--- src/main/java/numbers/Rational.java
+++ src/main/java/numbers/Rational.java
@@ -5,7 +5,24 @@ package numbers;
  *
  */
 public class Rational 
-{
+{   
+    private int numerator;
+    private int denominator;
+
+    public Rational() {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+
+    public Rational(int i) {
+        this.numerator = i;
+        this.denominator = 1;
+    }
+
+    public int numerator() { return numerator; }
+
+    public int denominator() { return denominator; }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- src/test/java/numbers/RationalTest.java
+++ src/test/java/numbers/RationalTest.java
@@ -3,6 +3,8 @@ package numbers;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Unit test for simple Rational.
@@ -33,6 +35,23 @@ public class RationalTest
      */
     public void testRational()
     {
-        assertTrue( true );
+        // Given no preconditions
+
+        // When I create a default `Rational` value
+        Rational value = new Rational();
+        // Then the value should have numerator 0
+        assertThat("the numerator should be 0", value.numerator(), is(0));
+        // And the value should have denominator 1
+        assertThat("the denominator should be 1", value.denominator(), is(1));
+    }
+
+    public void testConstructorInt()
+    {
+        // Given that I have constructed a `Rational` value using the argument `2`
+        Rational value = new Rational(2);
+        // Then the value should have numerator `2`
+        assertThat("the numerator should be 2", value.numerator(), is(2));
+        // And the value should have denominator `1`
+        assertThat("the denominator should be 1", value.denominator(), is(1));
     }
 }
